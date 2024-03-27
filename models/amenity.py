37327dcc0d4c08f9b0ba5e33e93357542f1240e8
ksyv <viais.kevin@hotmail.fr>
2024-03-27 18:16:08 +0100

    fix typo "s"

--- models/amenity.py
+++ models/amenity.py
@@ -16,5 +16,5 @@ class Amenity(BaseModel, Base):
     """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity",
+    places_amenities = relationship("Place", secondary="place_amenity",
                                    viewonly=False)

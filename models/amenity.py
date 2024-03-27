65fe3768af4efaf89c134676b7d30f380bd0cdd7
ksyv <viais.kevin@hotmail.fr>
2024-03-27 18:20:26 +0100

    delete last arg

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ This module defines the class Amenity """
-from models.base_model import BaseModel, Base
+from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
@@ -16,5 +16,4 @@ class Amenity(BaseModel, Base):
     """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    places_amenities = relationship("Place", secondary="place_amenity",
-                                   viewonly=False)
+    places_amenities = relationship("Place", secondary="place_amenity")

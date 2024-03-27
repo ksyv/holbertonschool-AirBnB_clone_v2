a12c4c89592da945cb7e3fe39806bcb538282062
ksyv <viais.kevin@hotmail.fr>
2024-03-27 17:13:58 +0100

    try

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -80,3 +80,9 @@ class DBStorage:
             bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+        def close(self):
+            """
+            Call remove() method on the private session attribute
+            """
+            self.__session.remove()

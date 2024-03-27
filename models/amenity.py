fd0d9993e4a3290b7440466cad028afe678fd9e4
ksyv <viais.kevin@hotmail.fr>
2024-03-27 18:23:20 +0100

    try with this typo

--- models/amenity.py
+++ models/amenity.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-""" This module defines the class Amenity """
-from models.base_model import Base, BaseModel
-from sqlalchemy import Column, String
+""" State Module for HBNB project """
 from sqlalchemy.orm import relationship
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String, ForeignKey
 
 
 class Amenity(BaseModel, Base):
-    """ Represents an amenity for a MySQL database.
-
-    Public class atributes:
-        __tablename__ (str): Name of MySQL table to store amnities.
-
-        name (Columns: Str): Name of the city.
-        state_id (Columns: Str): Foreign key to 'states.id'.
     """
-    __tablename__ = "amenities"
+    Amenity inherits from BaseModel and Base (respect the order)
+    """
+    __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    places_amenities = relationship("Place", secondary="place_amenity")
+    places_amenities = relationship(
+        "Place",
+        secondary="place_amenity")

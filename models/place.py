707f9f872790a858e733755168b92590b00cc7e1
ksyv <viais.kevin@hotmail.fr>
2024-03-27 18:34:37 +0100

    hjgf

--- models/place.py
+++ models/place.py
@@ -9,22 +9,10 @@ from models.review import Review
 import models
 from os import getenv
 
-place_amenity = Table(
-    'place_amenity', Base.metadata,
-    Column(
-        'place_id',
-        String(60),
-        ForeignKey('places.id'),
-        primary_key=True,
-        nullable=False),
-    Column(
-        'amenity_id',
-        String(60),
-        ForeignKey('amenities.id'),
-        primary_key=True,
-        nullable=False)
+place_amenity = Table('place_amenity', Base.metadata,
+    Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+    Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
 )
-
 
 class Place(BaseModel, Base):
     """ A place to stay """

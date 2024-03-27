42831d821835b58ebf39d0fd0211674baf66167d
ksyv <viais.kevin@hotmail.fr>
2024-03-27 18:25:59 +0100

    delete last change

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from sqlalchemy.orm import relationship
+""" This module defines the class Amenity """
 from models.base_model import Base, BaseModel
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel, Base):
+    """ Represents an amenity for a MySQL database.
+
+    Public class atributes:
+        __tablename__ (str): Name of MySQL table to store amnities.
+
+        name (Columns: Str): Name of the city.
+        state_id (Columns: Str): Foreign key to 'states.id'.
     """
-    Amenity inherits from BaseModel and Base (respect the order)
-    """
-    __tablename__ = 'amenities'
+    __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    places_amenities = relationship(
-        "Place",
-        secondary="place_amenity")
+    places_amenities = relationship("Place", secondary="place_amenity")

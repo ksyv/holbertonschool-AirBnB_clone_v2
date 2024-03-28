ef883f823ee49dc97a60687dcd33a965d976be01
ksyv <viais.kevin@hotmail.fr>
2024-03-28 10:31:56 +0100

    try to delete a line

--- models/amenity.py
+++ models/amenity.py
@@ -16,4 +16,3 @@ class Amenity(BaseModel, Base):
     """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    places_amenities = relationship("Place", secondary="place_amenity")

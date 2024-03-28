9548ce27b6bd3f846bdd169c182242d5385a7a9b
ksyv <viais.kevin@hotmail.fr>
2024-03-28 09:41:09 +0100

    delete last change

--- models/place.py
+++ models/place.py
@@ -41,7 +41,7 @@ class Place(BaseModel, Base):
         cascade="all, delete, delete-orphan"
     )
     amenities = relationship("Amenity",
-                             secondary='places_amenity', viewonly=False)
+                             secondary='place_amenity', viewonly=False)
 
     @property
     def reviews(self):

9cc34961a5527627eb5272faf05c65acd580debe
ksyv <viais.kevin@hotmail.fr>
2024-04-08 13:10:21 +0200

    add task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module for start a web aplication via Flask
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """home page"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """hbnb page"""
+    return "HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

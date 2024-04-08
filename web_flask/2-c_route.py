a06d292a8091e8aae7007a1754165d29163f0eff
ksyv <viais.kevin@hotmail.fr>
2024-04-08 13:27:12 +0200

    add com'

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,7 +20,7 @@ def hbnb():
 
 @app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
-    """c/<text> page"""
+    """c/<text> page (variable page)"""
     return "C {}".format(text.replace("_", " "))
 
 

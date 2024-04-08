48bb7859c6116bda3a2fcb9d69529495afbd3708
ksyv <viais.kevin@hotmail.fr>
2024-04-08 11:54:27 +0200

    delete html balise

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,8 +8,9 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_HBNB():
-    return "<p>Hello HBNB!</p>"
+    """home page"""
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port=5000)

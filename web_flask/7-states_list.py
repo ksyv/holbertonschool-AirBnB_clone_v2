3cc6aa79c41244523a7c8529cc136265525bd6c8
ksyv <viais.kevin@hotmail.fr>
2024-04-09 16:01:29 +0200

    delete last change

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web application"""
-
+"""
+Module for start a web aplication via Flask
+"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
 def close(self):
-    """ clse the session """
+    """Number_template/<n> page"""
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route("/states_list", strict_slashes=False)
 def states_list():
-    """Display States"""
+    """Number_template/<n> page"""
     states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

a7b44a2d3cfb8daf08030af47366bdff6cab52a3
ksyv <viais.kevin@hotmail.fr>
2024-04-09 16:13:30 +0200

    delete last change

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,35 +1,24 @@
 #!/usr/bin/python3
-# File: 7-states_list.py
-# Authors: Yoshua Lopez - Ma Paz Quirola -Laura Socarras
-
-""""
-Script starts Flask web app
-    listen on 0.0.0.0, port 5000
-    routes: /:
-            /states_list: Display HTML and state info from storage;
 """
-
-from email.policy import strict
+Module for start a web aplication via Flask
+"""
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
-
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_db(self):
-    """After each request remove current SQLAlchemy session"""
+def close(self):
+    """Number_template/<n> page"""
     storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-       States are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+    """Number_template/<n> page"""
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == "__main__":

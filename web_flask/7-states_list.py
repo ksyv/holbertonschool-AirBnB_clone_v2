2e9fe13330995c2c5777891045eb1b54c761831f
ksyv <viais.kevin@hotmail.fr>
2024-04-09 14:10:23 +0200

    add files for task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module for start a web aplication via Flask
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close(self):
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

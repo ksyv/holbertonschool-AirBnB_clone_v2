a812fc8ae2970f5f4ef5d2335af0555a3de97e4d
ksyv <viais.kevin@hotmail.fr>
2024-04-09 10:47:43 +0200

    add a missing slash

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -43,7 +43,7 @@ def number_template(n):
     return render_template("5-number.html", n=n)
 
 
-@app.route("number_odd_or_even/<int:n>", strict_slashes=False)
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even_template(n):
     """Number_template/<n> page"""
     return render_template("6-number_odd_or_even.html", n=n)

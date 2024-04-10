cd2f6c914bc86de3eb2eccdad95e5be0715e8fab
ksyv <viais.kevin@hotmail.fr>
2024-04-10 11:44:41 +0200

    delete last change

--- models/base_model.py
+++ models/base_model.py
@@ -17,19 +17,23 @@ class BaseModel:
     updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
 
     def __init__(self, *args, **kwrgs):
-        """Initialize a new BaseModel.
-        Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
-        """
-        self.id = str(uuid4())
-        self.created_at = self.updated_at = datetime.now()
-        if kwrgs:
-            for key, value in kwrgs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
-                    setattr(self, key, value)
+        """Instatntiates a new model"""
+        if not kwrgs:
+            from models import storage
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+        else:
+            if 'updated_at' in kwrgs:
+                kwrgs['updated_at'] = datetime.strptime(kwrgs['updated_at'],
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+            if 'created_at' in kwrgs:
+                kwrgs['created_at'] = datetime.strptime(kwrgs['created_at'],
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+            if '__class__' in kwrgs:
+                del kwrgs['__class__']
+            self.__dict__.update(kwrgs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
